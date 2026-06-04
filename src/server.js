c53519dbf946f5e9243631d98e64dd3de52a8ab1
Habiba <enghabibahammouda@gmail.com>
2026-06-05 02:15:29 +0300

    fixed CORS setup

--- src/server.js
+++ src/server.js
@@ -30,14 +30,25 @@ app.listen(PORT, () => {
   simulation.start();
 });
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://gp-front-9abbxv1vv-habibas-projects-e7971660.vercel.app/dashboard'
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://gp-front-9abbxv1vv-habibas-projects-e7971660.vercel.app/dashboard'
+// ];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+//     else callback(new Error('Not allowed by CORS'));
+//   }
+// }));
 
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) callback(null, true);
-    else callback(new Error('Not allowed by CORS'));
+    // Allow localhost and all vercel.app deployments
+    if (!origin || origin.includes('localhost') || origin.includes('vercel.app')) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
   }
 }));
